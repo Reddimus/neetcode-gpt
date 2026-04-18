19d05e9a17d4d1bb5fdbca1ec4ea94de91bc9305
Kevin M <103332900+Reddimus@users.noreply.github.com>
2026-04-18 15:57:51 -0700

    Update Gradient descent optimizer (Math Foundations 1/6)

--- foundations/gradient_descent.py
+++ foundations/gradient_descent.py
@@ -1,6 +1,11 @@
 class Solution:
     def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
+        # Objective function: f(x) = x^2
+        # Derivative:         f'(x) = 2x
+        # Update rule:        x = x - learning_rate * f'(x)
+        # Round final answer to 5 decimal places
+        x: int = init # current guess value
         for i in range(iterations):
-            d: int = 2 * init
-            init -= d * learning_rate
-        return round(init, 5)
+            d: int = x * 2 # Gradient: df/dx at current x guess
+            x -= learning_rate * d # Gradient step: new guess = old guess - (α * derivative)
+        return round(x, 5)
